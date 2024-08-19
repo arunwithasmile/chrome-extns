8e3e5b749792714c0d051e9b233171080f65c11c
arunwithasmile <arunwithasmile@gmail.com>
2024-08-19 12:29:44 +0530

    Modularising Files

--- wiki/content.js
+++ wiki/content.js
@@ -3,4 +3,13 @@ chrome.storage.sync.get('active', function (data) {
     if (data.active) {
         document.body.classList.add('asp-wiki-minify');
     }
+    var link = document.createElement("link");
+    link.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200";
+    link.type = "text/css";
+    link.rel = "stylesheet";
+
+    document.getElementsByTagName("head")[0].appendChild(link);
+
+    document.querySelector('.mw-ui-icon-wikimedia-appearance').classList.add('material-symbols-outlined');
+    document.querySelector('.mw-ui-icon-wikimedia-appearance').textContent = 'palette';
 });
